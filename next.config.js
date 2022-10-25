e92a87ef1d847caef27955e431cb3edf96f8e13d
alihaider1998 <67502924+alihaider1998@users.noreply.github.com>
2022-10-25 16:46:00 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
-  // basePath: "/Test-deepsynthbody",
-  // assetPrefix: "/Test-deepsynthbody", 
+  basePath: "/Test-deepsynthbody",
+  assetPrefix: "/Test-deepsynthbody", 
 }
 
 module.exports = nextConfig
