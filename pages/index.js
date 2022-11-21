debc45c30f8f9d900152c233483dd70ffaa121ac
Raja Ali Haider <rajaalihaider@Rajas-MacBook-Pro.local>
2022-11-21 13:48:49 +0100

    new style

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import { SearchContext } from '../components/context/search'
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from 'react'
 import { clearLinks } from "../slices/sidebarStatus";
+import Header from "../components/header";
+import { useRouter } from "next/router";
 
 
 export async function getStaticProps() {
@@ -21,21 +23,29 @@ export async function getStaticProps() {
             frontmatter
         }
     })
+    const readFile = fs.readFileSync(`CategoryImages/categoryImages.md`, 'utf-8')
+    const { data: frontmatter } = matter(readFile)
+    const images=frontmatter
     return {
         props: {
-            filesData
+            filesData,
+            images
         }
     }
 }
 
-export default function Home({ filesData }) {
+export default function Home({ filesData, images}) {
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(clearLinks())
     })
-
     const [value, setValue] = useState('');
+
+
+
+
+
     return (
         <SearchContext.Provider value={{ value, setValue }}>
 
@@ -46,7 +56,14 @@ export default function Home({ filesData }) {
                     </h2></div>
                     <SearchAndFilter />
                 </div>
-                <DatasetGrid filesData={filesData} />
+                {useRouter().pathname == "/" ? (
+            <header className="mb-8 py-2 justify-center">
+              <Header />
+            </header>
+          ) : (
+            false
+          )}
+                <DatasetGrid filesData={filesData} images={images} />
             </div>
         </SearchContext.Provider>
 
